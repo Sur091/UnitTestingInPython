79d1edcc9b4679a3c74606788911e8982b891f4a
Sur091 <sacharya27@wooster.edu>
2024-08-28 12:39:10 -0400

    Working unit test, one method and two classes

--- test.py
+++ test.py
@@ -1,44 +1,59 @@
 import unittest
 
-class Soobin(unittest.TestCase):
+class Soobin:
 
     def get_name(self) -> str:
         return "Soobim"
     
-    # Get the number of primes below 'n'
-    def number_of_primes_below(n: int):
-        import numpy as np
-        length_of_sieve = int(n - 1) // 2
+    # # Get the number of primes below 'n'
+    # def number_of_primes_below(n: int):
+    #     import numpy as np
+    #     length_of_sieve = int(n - 1) // 2
 
-        # A boolean list for odd numbers
-        primes_list = np.full(length_of_sieve, True)
+    #     # A boolean list for odd numbers
+    #     primes_list = np.full(length_of_sieve, True)
 
-        index_of_square_root_of_n: int = int(n ** 0.5) // 2
+    #     index_of_square_root_of_n: int = int(n ** 0.5) // 2
 
-        # Sieve
-        for a in range(index_of_square_root_of_n):
-            if primes_list[a]:
-                index_of_a_squared: int = 2*a*a + 6*a + 3
-                step: int = 2*a + 3
-                primes_list[index_of_a_squared::step] = False
+    #     # Sieve
+    #     for a in range(index_of_square_root_of_n):
+    #         if primes_list[a]:
+    #             index_of_a_squared: int = 2*a*a + 6*a + 3
+    #             step: int = 2*a + 3
+    #             primes_list[index_of_a_squared::step] = False
 
-        # The odd number is prime if at the corresponding index we have the value True
-        return np.sum(primes_list) + 1
+    #     # The odd number is prime if at the corresponding index we have the value True
+    #     return np.sum(primes_list) + 1
     
-    def test_name(self) -> None:
-        self.assertEqual(self.get_name(), "Soobim")
+    # def test_name(self) -> None:
+    #     self.assertEqual(self.get_name(), "Soobim")
+
+    # def test_primes(self) -> None:
+    #     tests = [(10, 4), (100, 25), (1000, 168), (10**4, 1229), (10**5, 9592), (10**6, 78498), (10**7, 664579)]
+
+    #     for (n, v) in tests:
+    #         self.assertEqual(v, self.number_of_primes_below(n))
     
-    def test_fibbonacci(self, n: int) -> int:
+    # get a fibbonacci number
+    def fibb(self, n: int) -> int:
         a, b = 0, 1
         for _ in range(n):
             a, b = b, a+b
         return a
 
-    def test_primes(self) -> None:
-        tests = [(10, 4), (100, 25), (1000, 168), (10_000, 1229), (100_000, 9592), (10**6, 78498), (10**7, 664579)]
 
-        for (n, v) in tests:
-            self.assertEqual(v, self.number_of_primes_below(n))
+class FibbonacciTesting(unittest.TestCase):
+
+    def test_fibbonacci(self):
+        tests = [(0, 0), (1, 1), (2, 1), (3, 2)]
+        soobin = Soobin()
+
+        for (n, value) in tests:
+            self.assertEqual(value, soobin.fibb(n))
+    
+    def test_name(self):
+        soobin = Soobin()
+        self.assertNotEqual(soobin.get_name(), "Soobim")
 
 
 if __name__ == '__main__':
